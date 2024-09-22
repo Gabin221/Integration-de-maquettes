81a12ac8bf16dcbacce3cf885a414efdafda5fda
Gabin221 <serrurot19@gmail.com>
2024-09-22 19:12:34 +0200

    Update

--- Easy payment app/app/src/main/java/com/example/easypaymentapp/ui/home/HomeFragment.kt
+++ Easy payment app/app/src/main/java/com/example/easypaymentapp/ui/home/HomeFragment.kt
@@ -1,5 +1,10 @@
 package com.example.easypaymentapp.ui.home
 
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.text.SpannableString
 import android.text.Spanned
@@ -7,7 +12,6 @@ import android.text.style.ImageSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -22,6 +26,16 @@ class HomeFragment : Fragment() {
 
     lateinit var eyeButton: TextView
     lateinit var shareButton: TextView
+    lateinit var allButton: TextView
+    lateinit var activityButton: TextView
+    lateinit var desktopButton: TextView
+    lateinit var mobileButton: TextView
+    lateinit var tabletButton: TextView
+    lateinit var paypalLogo: TextView
+    lateinit var starButton: TextView
+    lateinit var lightShareButton: TextView
+
+    lateinit var test: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,8 +47,30 @@ class HomeFragment : Fragment() {
 
         eyeButton = root.findViewById(R.id.eyeButton)
         shareButton = root.findViewById(R.id.shareButton)
+        allButton = root.findViewById(R.id.allButton)
+        activityButton = root.findViewById(R.id.activityButton)
+        desktopButton = root.findViewById(R.id.desktopButton)
+        mobileButton = root.findViewById(R.id.mobileButton)
+        tabletButton = root.findViewById(R.id.tabletButton)
+        paypalLogo = root.findViewById(R.id.paypalLogo)
+        starButton = root.findViewById(R.id.starButton)
+        lightShareButton = root.findViewById(R.id.lightShareButton)
+        test = root.findViewById(R.id.test)
 
         ajoutImage()
+
+        // Crée un dégradé de couleurs
+        val gradientDrawable = GradientDrawable(
+            GradientDrawable.Orientation.TOP_BOTTOM, // Choisir l'orientation du dégradé
+            intArrayOf(
+                Color.parseColor("#85fc6c"),
+                Color.parseColor("#f3f54c")
+            )
+        )
+
+// Applique le dégradé à l'arrière-plan du TextView
+        gradientDrawable.cornerRadius = 20f
+        allButton.background = gradientDrawable
 
         return root
     }
@@ -55,6 +91,54 @@ class HomeFragment : Fragment() {
         val imageSpanStartShare = ImageSpan(drawableShare!!, ImageSpan.ALIGN_BOTTOM)
         spannableString.setSpan(imageSpanStartShare, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         shareButton.text = spannableString
+
+        val drawableAll = ContextCompat.getDrawable(requireContext(), R.drawable.ic_pie_black_24dp)
+        drawableAll?.setBounds(0, 0, drawableAll.intrinsicWidth, drawableAll.intrinsicHeight)
+        val imageSpanStartAll = ImageSpan(drawableAll!!, ImageSpan.ALIGN_BOTTOM)
+        spannableString.setSpan(imageSpanStartAll, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        allButton.text = spannableString
+
+        val drawableActivity = ContextCompat.getDrawable(requireContext(), R.drawable.ic_chart_line_black_24dp)
+        drawableActivity?.setBounds(0, 0, drawableActivity.intrinsicWidth, drawableActivity.intrinsicHeight)
+        val imageSpanStartActivity = ImageSpan(drawableActivity!!, ImageSpan.ALIGN_BOTTOM)
+        spannableString.setSpan(imageSpanStartActivity, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        activityButton.text = spannableString
+
+        val drawableDesktop = ContextCompat.getDrawable(requireContext(), R.drawable.ic_monitor_black_24dp)
+        drawableDesktop?.setBounds(0, 0, drawableDesktop.intrinsicWidth, drawableDesktop.intrinsicHeight)
+        val imageSpanStartDesktop = ImageSpan(drawableDesktop!!, ImageSpan.ALIGN_BOTTOM)
+        spannableString.setSpan(imageSpanStartDesktop, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        desktopButton.text = spannableString
+
+        val drawableMobile = ContextCompat.getDrawable(requireContext(), R.drawable.ic_phone_check_menu_black_24dp)
+        drawableMobile?.setBounds(0, 0, drawableMobile.intrinsicWidth, drawableMobile.intrinsicHeight)
+        val imageSpanStartMobile = ImageSpan(drawableMobile!!, ImageSpan.ALIGN_BOTTOM)
+        spannableString.setSpan(imageSpanStartMobile, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        mobileButton.text = spannableString
+
+        val drawableTablet = ContextCompat.getDrawable(requireContext(), R.drawable.ic_tablet_black_24dp)
+        drawableTablet?.setBounds(0, 0, drawableTablet.intrinsicWidth, drawableTablet.intrinsicHeight)
+        val imageSpanStartTablet = ImageSpan(drawableTablet!!, ImageSpan.ALIGN_BOTTOM)
+        spannableString.setSpan(imageSpanStartTablet, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        tabletButton.text = spannableString
+
+        val drawablePaypal = ContextCompat.getDrawable(requireContext(), R.drawable.ic_paypal_black_24dp)
+        drawablePaypal?.setBounds(0, 0, drawablePaypal.intrinsicWidth, drawablePaypal.intrinsicHeight)
+        val imageSpanStartPaypal = ImageSpan(drawablePaypal!!, ImageSpan.ALIGN_BOTTOM)
+        spannableString.setSpan(imageSpanStartPaypal, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        paypalLogo.text = spannableString
+
+        val drawableStar = ContextCompat.getDrawable(requireContext(), R.drawable.ic_star_menu_black_24dp)
+        drawableStar?.setBounds(0, 0, drawableStar.intrinsicWidth, drawableStar.intrinsicHeight)
+        val imageSpanStartStar = ImageSpan(drawableStar!!, ImageSpan.ALIGN_BOTTOM)
+        spannableString.setSpan(imageSpanStartStar, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        starButton.text = spannableString
+
+        val drawableLightShare = ContextCompat.getDrawable(requireContext(), R.drawable.ic_light_share_black_24dp)
+        drawableLightShare?.setBounds(0, 0, drawableLightShare.intrinsicWidth, drawableLightShare.intrinsicHeight)
+        val imageSpanStartLightShare = ImageSpan(drawableLightShare!!, ImageSpan.ALIGN_BOTTOM)
+        spannableString.setSpan(imageSpanStartLightShare, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        lightShareButton.text = spannableString
     }
 
     override fun onDestroyView() {
