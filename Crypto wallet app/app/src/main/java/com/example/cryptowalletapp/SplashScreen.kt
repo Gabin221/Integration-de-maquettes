a7229b942f081f8486c183d71a5f1bd514f2dee4
Gabin221 <serrurot19@gmail.com>
2024-10-26 13:01:43 +0200

    Update

--- Crypto wallet app/app/src/main/java/com/example/cryptowalletapp/SplashScreen.kt
+++ Crypto wallet app/app/src/main/java/com/example/cryptowalletapp/SplashScreen.kt
@@ -1,27 +1,20 @@
 package com.example.cryptowalletapp
 
 import android.content.Intent
-import android.graphics.LinearGradient
-import android.graphics.Shader
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.WindowManager
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import kotlinx.coroutines.*
 
 @Suppress("DEPRECATION")
 class SplashScreen : AppCompatActivity() {
 
-    lateinit var titleH1: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        titleH1 = findViewById(R.id.titleH1)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -31,20 +24,12 @@ class SplashScreen : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 2)
 
         CoroutineScope(Dispatchers.Main).launch {
-            delay(2000)
+            delay(2)
             startActivity(Intent(this@SplashScreen, MainActivity::class.java))
             finish()
         }
-
-        val paint = titleH1.paint
-        val width = paint.measureText(titleH1.text.toString())
-        val textShader: Shader = LinearGradient(0f, 0f, width, titleH1.textSize, intArrayOf(
-            ContextCompat.getColor(this, R.color.startDegradeGreen),
-            ContextCompat.getColor(this, R.color.endDegradeYellow)
-        ), null, Shader.TileMode.CLAMP)
-        paint.shader = textShader
     }
 }
